51cc34bc81ebcace6f551d3de610892354133efd
Mateus Santos <a.mateussm1.5@gmail.com>
2022-11-06 20:12:30 -0300

    :tada: | Create final pdf

--- app/__main__.py
+++ app/__main__.py
@@ -3,6 +3,10 @@ import json
 
 file_path: str = r'C:\Users\Mateus\projects\python\generate_paper_page\src\data\code_for_begginers.json'
 
+# --------- EXTERNAL PACKAGES ---------
+from fpdf import FPDF
+import qrcode
+
 with open(
     file=file_path,
     mode='r',
@@ -10,15 +14,60 @@ with open(
 ) as file_: 
     content: list = json.load(file_)
 
-for data in content['data']:
-    title: str = data['title']
-    description: str = data['description']
-    urls: list[dict[str, str]] = data['urls']
-    print(f'Title: {title}')
-    print(f'Description: {description}')
-    
-    for url in urls:
-        print(f'\tTitle_url: {url["title"]}')
-        print(f'\tUrl: {url["url"]}')
+pdf = FPDF()
+pdf.add_page()
 
-    print('-'*80, end='\n\n')
+
+for data in content['data']:
+    pdf.set_font("Arial", size = 16, style='B')
+    title: str = data['title']
+    pdf.cell(185, 10, txt = title,
+            ln = 1, align = 'C')
+
+    pdf.set_font("Arial", size = 12, style='')
+    description: str = data['description']
+    pdf.multi_cell(185, 10, txt = description, align = 'J')
+    urls: list[dict[str, str]] = data['urls']
+
+    for url in urls:
+        pdf.set_font("Arial", size = 12, style='B')
+        pdf.cell(
+            w=200,
+            h=10,
+            txt='\n',
+            ln=1, align='C'
+        )
+        pdf.cell(
+            w=200,
+            h=10,
+            txt=f'{url["title"]}',
+            ln=1, align='C'
+        )
+        image_filename: str = 'image.png'
+        img = qrcode.make(
+            data=url["url"],
+            box_size=2
+        )
+        img.save(image_filename)
+
+        pdf.image(
+            name=image_filename,
+            x=90
+        )
+        for i in range(2):
+            pdf.cell(
+                w=200,
+                h=10,
+                txt='\n',
+                ln=1, align='C'
+            )
+
+pdf.set_font("Arial", size = 6, style='')
+final_maessage: str = 'Esse pdf foi gerado utilizando Python e práticas de programação horríveis.Não reproduzir em casa. =D'
+pdf.cell(
+    w=200,
+    h=10,
+    txt=final_maessage,
+    ln=1, align='C'
+)
+pdf.output("GFG.pdf")  
